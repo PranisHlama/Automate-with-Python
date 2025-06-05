6f5a0f4e8a22789bc860b7d64b6e66c4161e87a6
PranisHlama <pranishlama17@gmail.com>
2025-06-05 14:14:27 +0000

    Weather forcasting

--- working_with_api/weather_forcast.py
+++ working_with_api/weather_forcast.py
@@ -1,8 +1,16 @@
 import requests
 
-def get_weather_forcast(lat, lon, api= '3083522c86dd9b7d7d03e06fa13dd53a'):
-    url = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api}',
+# print("city,    Time,    Temperature,    Condition")
+def get_weather_forcast(cityname, api_key= '3083522c86dd9b7d7d03e06fa13dd53a'):
+    url = f'http://api.openweathermap.org/data/2.5/forecast?q={cityname}&appid={api_key}'
     r = requests.get(url)
     content = r.json()
+    open('data.txt', 'w').close()
+    with open('data.txt', 'a') as file:
+        file.write('datetime,  celcius,  condition, description \n')
+        for dicty in content['list']:
+            file.write(f"{dicty['dt_txt']}, {dicty['main']['temp']}, {dicty['weather'][0]['main']}, {dicty['weather'][0]['description']}\n")
+        # return content
+    
 
-print(get_weather_forcast('44.34', '10.99'))
+print(get_weather_forcast('Kathmandu'))
